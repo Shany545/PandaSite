f664657c816279a7b491d0a9cc52f2bbf0c0dfc8
Shany545 <31135014+Shany545@users.noreply.github.com>
2018-07-05 13:15:34 -0500

    Add files via upload

---
+++ jayda.js
@@ -0,0 +1,8 @@
+function butoone(){
+  var name = window.prompt("What's your name");
+  window.alert("Hello "+name);
+}
+
+
+document.getElementById("button1").addEventListener("click",butoone);
+//document.getElementById("flippy").addEventListener("click",downloadThis1);
